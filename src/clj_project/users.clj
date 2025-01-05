5966e5c1e3b3d627e2b5b84551b22f13272739bc
Natalija Brankovic <brankovic.natalija01@gmail.com>
2025-01-05 20:56:07 +0100

    Add functions for adding, finding, and deleting users in thedatabase

--- src/clj_project/users.clj
+++ src/clj_project/users.clj
@@ -1,4 +1,6 @@
-(ns clj-project.users)
+(ns clj-project.users
+  (:require [monger.collection :as mc]
+            [clj-project.db :refer [db]]))
 
 (def users (atom {}))
 
@@ -19,3 +21,30 @@
       (swap! users dissoc username)
       (println (str "User with username '" username "' has been removed.")))
     (println (str "User with username '" username "' does not exist."))))
+
+(defn add-user-db [name surname username password email]
+  (if (mc/find-one-as-map @db "users" {:username username})
+    (println (str "User with username '" username "' already exists."))
+    (do
+      (mc/insert @db "users" {:name name
+                              :surname surname
+                              :username username
+                              :password password
+                              :email email
+                              :friends []})
+      (println (str "User '" username "' added successfully to the database.")))))
+
+(defn get-user-db [username]
+  (let [user (mc/find-one-as-map @db "users" {:username username})]
+    (if user
+      (do
+        (println (str "Found user in database: " user))
+        user)
+      (println (str "No user found in database with username '" username "'")))))
+
+(defn remove-user-db [username]
+  (if (mc/find-one-as-map @db "users" {:username username})
+    (do
+      (mc/remove @db "users" {:username username})
+      (println (str "User with username '" username "' has been removed from the database.")))
+    (println (str "User with username '" username "' does not exist in the database."))))
