96bbea4b5129849e01b11678c190c81a9ff4bb7b
Natalija Brankovic <brankovic.natalija01@gmail.com>
2025-01-05 20:45:42 +0100

    Connect to MongoDB

--- src/clj_project/core.clj
+++ src/clj_project/core.clj
@@ -1,7 +1,8 @@
 (ns clj-project.core
   (:require [clj-project.users :as users]
             [clj-project.friends :as friends]
-            [clj-project.expenses :as expenses]))
+            [clj-project.expenses :as expenses]
+            [clj-project.db :as db]))
 
 (defn -main []
   (println "Welcome to the Expense Sharing App!")
@@ -9,3 +10,7 @@
   (users/add-user "Pera" "Peric" "perapera" "password123" "pera.pera@example.com")
   (friends/add-friend "perapera" "jovanovic" "Jovan" "Jovanovic" "jovan@example.com" 300)
   (expenses/add-expense "perapera" "jovanovic" 200 "perapera" "2025-01-01" "Dinner"))
+
+
+(db/connect!)
+(db/disconnect!)
