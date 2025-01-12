31d6664b021c66810f3c50d35fa0fee2e6c3a6eb
Natalija Brankovic <brankovic.natalija01@gmail.com>
2025-01-12 11:01:38 +0100

    Removed files from tracking based on .gitignore

--- resources/public/js/cljs-runtime/cljs_project.components.home_page.js
+++
@@ -1,6 +0,0 @@
-goog.provide('cljs_project.components.home_page');
-cljs_project.components.home_page.home_page = (function cljs_project$components$home_page$home_page(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"display","display",242065432),"flex",new cljs.core.Keyword(null,"justify-content","justify-content",-1990475787),"center",new cljs.core.Keyword(null,"align-items","align-items",-267946462),"center",new cljs.core.Keyword(null,"height","height",1025178622),"100vh",new cljs.core.Keyword(null,"background-color","background-color",570434026),"#e8f5e9"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"Welcome to the Main Application Page!"], null)], null);
-});
-
-//# sourceMappingURL=cljs_project.components.home_page.js.map
--- resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { cljs_project.core.init(); } catch (e) { console.error("An error occurred when calling (cljs-project.core/init)"); console.error(e); }
